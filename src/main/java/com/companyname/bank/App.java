9961d72409d17f2460ab5b853901eea378026093
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2026-01-06 10:52:32 +0530

    test commit for webhook

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "another poll scm test commit");
+        System.out.println( "test commit for webhook");
     }
 }
